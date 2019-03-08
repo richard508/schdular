0f81d0dbdcdc4abacdb1bb752c10449c66b80ea0
Richard Almonte <richard.almonte@bridgesite.io>
2019-03-08 15:47:43 -0500

    create schedular commponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Schedular from './components/Schedular'
 
 class App extends Component {
   render() {
     return (
       <div className="App schedule">
         <h2 className="appointment">Select An Appointment</h2>
+        <Schedular />
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ client/src/components/Schedular.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Schedular extends Component {
+  render() {
+    return (
+      <div>
+        Schedular Component
+      </div>
+    );
+  }
+}
+
+export default Schedular;
