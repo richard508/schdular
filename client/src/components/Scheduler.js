60435f64d0a23918922d5b3a86376daeca195282
Richard Almonte <richard.almonte@bridgesite.io>
2019-03-09 16:17:36 -0500

    hide show modal

--- client/src/components/Scheduler.js
+++ client/src/components/Scheduler.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux"
 import {getPeople} from "../store/actions/scheduleActions"
+import BootModal from './Modal';
 
 class Scheduler extends Component {
+  state = {
+    openModal: false,
+  }
   componentDidMount(){
     this.props.getPeople()
   }
 
   openTime = (id) =>{
-    console.log(id)
+    this.setState(prevState => ({
+      openModal: !prevState.openModal
+    }))
   }
+
   render() {
     return (
-      <div className="appointment_times time_font">
-        {this.props.appointments && this.props.appointments.map((appointment, i) => (
-          <div
-            className={appointment.isAvailable === true ? 'time' : 'time taken_time'}
-            onClick={() => this.openTime(appointment._id)}
-            key={i}
-          >
-            {appointment.time}
-          </div>
-        ))}
+      <div>
+        <div className="appointment_times time_font">
+          {this.props.appointments && this.props.appointments.map((appointment, i) => (
+            <div
+              className={appointment.isAvailable === true ? 'time' : 'time taken_time'}
+              onClick={() => this.openTime(appointment._id)}
+              key={i}
+            >
+              {appointment.time}
+            </div>
+          ))}
+        <BootModal show={this.state.openModal} onHide={this.openTime}/>
+        </div>
       </div>
     )
   }
