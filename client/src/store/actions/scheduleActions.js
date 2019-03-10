f216a1ec595c6f8a930d41b5551b8f1f99767fbb
Richard Almonte <richard.almonte@bridgesite.io>
2019-03-10 13:37:32 -0400

    create preson and update appt

--- client/src/store/actions/scheduleActions.js
+++ client/src/store/actions/scheduleActions.js
@@ -11,3 +11,24 @@ export const getPeople = () => {
     })
   }
 }
+
+export const updateAppt = (id, payload) => {
+  return (dispatch) => {
+    axios.post(`api/people`, payload).then(res => {
+      const personPayload = {
+        person: res.data._id,
+        isAvailable: false
+      }
+      axios.patch(`/api/appointments/${id}`, personPayload).then(res => {
+        const data = res.data
+        dispatch({type: 'UPDATE_APPT',data})
+      })
+      .catch(error => {
+        dispatch({type: 'ERROR_APPT', error})
+      })
+    })
+    .catch(error => {
+      dispatch({type: 'ERROR_PERSON', error})
+    })
+  }
+}
