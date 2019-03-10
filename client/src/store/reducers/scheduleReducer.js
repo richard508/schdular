303e34132e2d8ef47160b6143d7a5ef9388af5bd
Richard Almonte <richard.almonte@bridgesite.io>
2019-03-10 13:38:27 -0400

    update state in redux

--- client/src/store/reducers/scheduleReducer.js
+++ client/src/store/reducers/scheduleReducer.js
@@ -1,11 +1,25 @@
 const initState = {
   appointments: null
 }
+
+function updateAppt (appointments, data) {
+  const newAppt = appointments.map(appointment => (
+    appointment._id === data._id 
+    ? {...appointment, isAvailable: false} 
+    : appointment))
+    return newAppt
+}
+
 const scheduleReducer = (state = initState, action) =>{
   switch(action.type){
     case 'GET_APPT':
       return {
         appointments: action.data
+      }
+    case 'UPDATE_APPT':
+      return {
+        ...state,
+        appointments: [...updateAppt(state.appointments, action.data)]
       }
     case 'ERROR_APPT':
       console.log('Error getting appointments')
